166cd2bee399395ed7c590c3ede18512259d4a70
leftmove <100anonyo@gmail.com>
2024-08-04 15:04:26 -0400

    enqueing refactor

--- test/index.ts
+++ test/index.ts
@@ -1,27 +1,5 @@
 import Facebook from "../src";
-import { Client } from "../src";
-import type { Permissions, Authentication } from "../src";
 
-// const facebook = new Facebook();
+const facebook = new Facebook();
 
-// const scope: Permissions = {
-//   pages_manage_engagement: true,
-//   pages_manage_posts: true,
-//   pages_read_engagement: true,
-//   pages_read_user_content: true,
-//   pages_show_list: true,
-//   read_insights: true,
-//   business_management: true,
-// };
-// const config: Authentication = {
-//   path: "./credentials.json",
-//   expireTime: 60,
-//   scope,
-// };
-
-// await facebook.authenticate(config);
-
-const client = new Client();
-
-const data = client.get("");
-console.log(data);
+facebook.verify();
