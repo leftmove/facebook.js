a8a68b0679bc06b9ccb3e57d994a778f1756802e
leftmove <100anonyo@gmail.com>
2024-08-04 15:04:26 -0400

    general refactor and error handling

--- test/index.ts
+++ test/index.ts
@@ -2,4 +2,5 @@ import { Facebook } from "../src";
 
 const zuck = new Facebook({ appId: "494601056372880", appSecret: "123" });
 
-const token = await zuck.refreshUserToken();
+const token = await zuck.getUserToken();
+console.log(token);
