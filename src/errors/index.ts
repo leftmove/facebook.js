e317be45e7a042fef046b627ab7606ba13f3acc4
leftmove <100anonyo@gmail.com>
2024-08-04 15:04:26 -0400

    error handling and user token callback through code

---
+++ src/errors/index.ts
@@ -0,0 +1,42 @@
+// Some really janky error handling here
+// I'm not sure what the best way to handle this is, but I'm pretty sure this isn't it
+
+export class GraphError extends Error {
+  status: number;
+  response: Response;
+  data: any;
+  constructor(response: Response, data: any, error: any = null) {
+    const message = response.statusText;
+    super(message);
+    const e = new Error(message);
+
+    data.url = response.url;
+    data.status = response.status;
+
+    this.data = data;
+    this.cause = error ? error.cause : e.cause;
+    this.stack = error ? error.stack : e.stack;
+
+    this.response = response;
+    this.status = data?.error?.code || response.status;
+
+    this.name = "GraphError";
+    this.message = `${message}\n${JSON.stringify(data, null, 2)}`;
+  }
+}
+
+export class CredentialError extends GraphError {
+  constructor(message: string, error: any) {
+    super(error.response, error.data, error);
+    this.message = `${error.message}\n${message}`;
+    this.name = "CredentialError";
+  }
+}
+
+export class UnauthorizedError extends GraphError {
+  constructor(message: string, error: any) {
+    super(error.response, error.data, error);
+    this.message = `${error.message}\n${message}`;
+    this.name = "UnauthorizedError";
+  }
+}
--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,8 @@
 import { Facebook } from "../src";
 
-const zuck = new Facebook({ appId: "494601056372880", appSecret: "123" });
+const zuck = new Facebook({
+  appId: "494601056372880",
+  appSecret: "ed1e95f80b0dc75b109e5fc9b2f45ebf",
+});
 
 const token = await zuck.getUserToken();
-console.log(token);
